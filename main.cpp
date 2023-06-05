413685f929760408228f53f4c5f7dfa4883532f1
Yejin <yejin01e@gmail.com>
2023-06-05 10:49:05 +0900

    Update main.cpp
    바 이동 (숫자 조금 더 보기)
    벽돌 깨지기 (하나 깨는데 한 줄이 다 같이 깨짐)
    
    + 추가할 것
    + 점수 시스템
    + 생명 시스템

--- main.cpp
+++ main.cpp
@@ -30,19 +30,28 @@ int		backTop = 670;
 
 int		polygon_num = 36;
 int		ball_num = 1;
-int		block_num = 50000;
+int		block_num = 100;
+
+int		scoreX = 1100;
+int		scoreW = 180;
 
 int		collide = 0;
 int		ranking[5] = { 234, 197, 120, 88, 36 };
 int		score = 12;
 int		time_limit = 321;
 
-
 float	barX;
 float	barY;
-int		barW = 70;
+int		barW = 200;
 int		barH = 15;
 
+int		brickWidth = 50;
+int		brickHeight = 30;
+
+float	delta_x, delta_y;
+float	fix_radius;
+
+int		ballColor[3][3] = { {0.0, 1.0, 1.0}, {1.0, 1.0, 0.0}, {1.0, 0.0, 1.0} };
 
 struct Point {
 	float	x;
@@ -60,11 +69,6 @@ struct Color {
 	float	b = 0.0;
 };
 
-struct Check {
-	Color	color;
-	int		count;
-};
-
 struct Ball {
 	Point	center;
 	float	radius = 10.0;
@@ -73,24 +77,18 @@ struct Ball {
 	Vector	velocity;
 };
 
-struct Block {
+struct Brick {
 	Point	point;
 	int		width = 50;
 	int		height = 30;
-	Color	color;
-	int		count;
-	//Check	count;		
+	int		count = 0;
 };
 
 struct Ball* ball;
-struct Block* block;
+struct Brick* brick;
 struct Check* check;
 struct Point* polygon_vertex;
 
-float	delta_x, delta_y;
-float	fix_radius;
-
-int		ballColor[3][3] = { {0.0, 1.0, 1.0}, {1.0, 1.0, 0.0}, {1.0, 0.0, 1.0} };
 
 void init(void) {
 	float	delta;
@@ -113,8 +111,8 @@ void init(void) {
 		//ball[i].radius = radius * (1.5 + sin((double)(i)));
 		ball[i].center.x = 230;
 		ball[i].center.y = 110;
-		ball[i].velocity.x = 0.5;
-		ball[i].velocity.y = 0.1;
+		ball[i].velocity.x = 1.0;
+		ball[i].velocity.y = 0.5;
 		ball[i].mass = 1;
 		ball[i].color.r = ballColor[i][0];
 		ball[i].color.g = ballColor[i][1];
@@ -123,7 +121,7 @@ void init(void) {
 
 	int n = 0;
 	float last = 0.0;
-	block = new Block[100];
+	brick = new Brick[100];
 	for (int j = 1; j <= 2; j++) {
 		a = 20 + rand() % 30;
 		k = i;
@@ -137,20 +135,12 @@ void init(void) {
 
 			if (((y + 2884) * 0.3031) < x + 50 + 5.0) { break; }
 
-			block[n].point.x = x;
-			block[n].point.y = y;
+			brick[n].point.x = x;
+			brick[n].point.y = y;
 
-			// Set Block Color
-			if (n % 11 == 6) { block[n].count = 0; }
-			else { block[n].count = rand() % 3 + 1; }
-			switch (block[i].count)
-			{
-			case 0:		block[i].color.r = 255.0;	block[i].color.g = 255.0;	block[i].color.b = 255.0;	break;
-			case 1:		block[i].color.r = 255.0;	break;
-			case 2:		block[i].color.g = 255.0;	break;
-			case 3:		block[i].color.b = 255.0;	break;
-			default:	break;
-			}
+			// Set Brick Color
+			if (n % 11 == 6) { brick[n].count = 0; }
+			else { brick[n].count = rand() % 7 + 1; }
 			n++;
 		}
 	}
@@ -193,24 +183,23 @@ void Draw_Background() {
 	glEnd();
 
 	// Score Background
-	//glColor3f(255.0, 255.0, 255.0);
-	//glBegin(GL_POLYGON);
-	//glVertex2f(scoreX, backBottom);
-	//glVertex2f(scoreX, backTop);
-	//glVertex2f(scoreX + scoreW, backTop);
-	//glVertex2f(scoreX + scoreW, backBottom);
-	//glEnd();
+	glColor3f(255.0, 255.0, 255.0);
+	glBegin(GL_POLYGON);
+	glVertex2f(scoreX, backBottom);
+	glVertex2f(scoreX, backTop);
+	glVertex2f(scoreX + scoreW, backTop);
+	glVertex2f(scoreX + scoreW, backBottom);
+	glEnd();
 }
 
 
-void Draw_Bar(int x, int y) {
+void Draw_Bar() {
 	glColor3f(0.5, 0.0, 1.0);
-
 	glBegin(GL_POLYGON);
-	glVertex3f(x, y, 0.0);
-	glVertex3f(x + 70, y, 0.0);
-	glVertex3f(x + 70, y - 15, 0.0);
-	glVertex3f(x, y - 15, 0.0);
+	glVertex3f(barX, barY, 0.0);
+	glVertex3f(barX + barW, barY, 0.0);
+	glVertex3f(barX + barW, barY - barH, 0.0);
+	glVertex3f(barX, barY - barH, 0.0);
 	glEnd();
 }
 
@@ -231,15 +220,32 @@ void Draw_Ball(int indexBall) {
 }
 
 
-void Draw_Block(int indexBlock) {
-	float	x, y, width, height;
+void Draw_Brick(int idx) {
+	float	x, y;
+	float	r = 0.0, g = 0.0, b = 0.0;
+	int		width, height, count;
 
-	x = block[indexBlock].point.x;
-	y = block[indexBlock].point.y;
-	width = block[indexBlock].width;
-	height = block[indexBlock].height;
+	x = brick[idx].point.x;
+	y = brick[idx].point.y;
+	width = brick[idx].width;
+	height = brick[idx].height;
+	count = brick[idx].count;
 
-	glColor3f(block[indexBlock].color.r, block[indexBlock].color.g, block[indexBlock].color.b);
+	switch (count)
+	{
+	case 0:		r = 1.0;	g = 1.0;	b = 1.0;	break;
+	case 1:		r = 1.0;							break;
+	case 2:		r = 1.0;	g = 0.5;				break;
+	case 3:		r = 1.0;	g = 1.0;				break;
+	case 4:		g = 1.0;							break;
+	case 5:		r = 0.5;	g = 0.8;	b = 1.0;	break;
+	case 6:		b = 1.0;							break;
+	case 7:		r = 0.6;	g = 0.1;	b = 0.9;	break;
+	default:
+		break;
+	}
+
+	glColor3f(r, g, b);
 
 	glBegin(GL_POLYGON);
 	glVertex3f(x, y, 0.0);
@@ -259,28 +265,141 @@ void MyReshape(int w, int h) {
 
 
 void Check_Collision_Ball_Wall(Ball& ball) {
-	if (ball.center.y <= 350.0) {
-		if (ball.center.y - ball.radius < 30.0 && ball.velocity.y < 0.0) {		// Bottom Face
+
+	float bx = ball.center.x;
+	float by = ball.center.y;
+	float br = ball.radius;
+
+	if (by <= height / 2) {
+		if (by - br < backBottom && by < 0.0) {		// Bottom Face
 			ball.velocity.y *= -1;
 		}
-		else if ((4 * ball.center.x + 50) < ball.center.y && ball.velocity.x < 0.0) {	// 1 Face
+		else if ((3.2 * (bx - br) - 34) < by && ball.velocity.x < 0.0) {		// 3 Face
 			ball.velocity.x *= -1;
 		}
-		else if (((-4) * ball.center.x + ball.radius + 1950) < ball.center.y && ball.velocity.x > 0.0) {	// 2 Face
+		else if ((-3.2 * (bx + br) + 3486) < by && ball.velocity.x > 0.0) {		// 4 Face
 			ball.velocity.x *= -1;
 		}
 	}
-	else if (ball.center.y > 350.0) {
-		if (ball.center.y + ball.radius > 670.0 && ball.velocity.y > 0.0) {		// Top Face
+	else if (by > height / 2) {
+		if (by + br > backTop && by > 0.0) {		// Top Face
 			ball.velocity.y *= -1;
 		}
-		else if (((-4) * ball.center.x + 750) > ball.center.y && ball.velocity.x < 0.0) {	// 3 Face
+		else if ((-3.3 * (bx - br) + 746) > by && ball.velocity.x < 0.0) {		// 2 Face
 			ball.velocity.x *= -1;
 		}
-		else if ((4 * ball.center.x - 1250) > ball.center.y && ball.velocity.x > 0.0) {		// 4 Face
+		else if ((3.3 * (bx + br) - 2884) > by && ball.velocity.x > 0.0) {		// 1 Face
 			ball.velocity.x *= -1;
 		}
 	}
+}
+
+
+bool Check_Collision_Ball_Brick() {
+	int i, j;
+	float bx, by, br, vx, vy;
+	float x, y, h, w;
+
+	for (i = 0; i < ball_num; i++) {
+		Ball b = ball[i];
+		bx = b.center.x;
+		by = b.center.y;
+		br = b.radius;
+		vx = b.velocity.x;
+		vy = b.velocity.y;
+
+		for (j = 0; j < block_num; j++) {
+			Brick k = brick[j];
+			x = k.point.x;		// 점 좌하단 기준 
+			y = k.point.y;
+			h = brick[0].height;
+			w = brick[0].width;
+
+
+			// 없는 벽돌이라면
+			if (k.count == 0) {
+				continue;
+			}
+
+
+			// 벽돌 하단 면
+			if (x <= bx && bx <= (x + w) && y <= (by + br) && vy > 0.0) {
+				//cout << "1";
+				ball[i].velocity.y *= -1;
+				brick[j].count--;
+				if (k.count > 0)	return true;
+			}
+
+			// 벽돌 우하단 면
+			if (x == (bx - br) && y == (by + br)) {
+				ball[i].velocity.x *= -1;
+				ball[i].velocity.y *= -1;
+				brick[j].count--;
+				if (k.count > 0)	return true;
+			}
+
+			// 벽돌 우단 면
+			if ((bx - br) <= (x + h) && y <= by && by <= (y + h) && vx < 0.0) {
+				//cout << "2";
+				ball[i].velocity.x *= -1;
+				brick[j].count--;
+				if (k.count > 0)	return true;
+			}
+
+			// 벽돌 우상단 면
+			if (x == (bx - br) && y == (by - br)) {
+				ball[i].velocity.x *= -1;
+				ball[i].velocity.y *= -1;
+				brick[j].count--;
+				if (k.count > 0)	return true;
+			}
+
+			// 벽돌 상단 면
+			if (x <= bx && bx <= (x + h) && y < (by - br) && (by - br) <= (y + h) && vy < 0.0) {
+				//cout << "3";
+				ball[i].velocity.y *= -1;
+				brick[j].count--;
+				if (k.count > 0)	return true;
+			}
+
+			// 벽돌 좌상단 면
+			if (x == (bx + br) && y == (by - br)) {
+				ball[i].velocity.x *= -1;
+				ball[i].velocity.y *= -1;
+				brick[j].count--;
+				if (k.count > 0)	return true;
+			}
+
+			// 벽돌 좌단 면
+			if (x <= (bx + br) && y <= by && by <= (y + h) && vy > 0.0) {
+				//cout << "4";
+				ball[i].velocity.x *= -1;
+				brick[j].count--;
+				if (k.count > 0)	return true;
+			}
+
+			// 벽돌 좌하단 면
+			if (x == (bx + br) && y == (by + br)) {
+				ball[i].velocity.x *= -1;
+				ball[i].velocity.y *= -1;
+				brick[j].count--;
+				if (k.count > 0)	return true;
+			}
+
+		}
+	}
+	return false;
+}
+
+void Check_Collision_Ball_Bar(Ball& ball) {
+	float bx = ball.center.x;
+	float by = ball.center.y;
+	float br = ball.radius;
+
+	if (barX < bx && bx < barX + barW && by + br < barY + barH) {
+		ball.velocity.y *= -1;
+	}
+
 }
 
 
@@ -293,11 +412,26 @@ void Update_Position(void) {
 
 
 void SpecialKey(int key, int x, int y) {
+	switch (key) {
+	case GLUT_KEY_LEFT:		
+		barX -= 50;
+		break;
+	case GLUT_KEY_RIGHT:	
+		barX += 50;
+		break;
+	//case GLUT_KEY_DOWN:		moving_sphere.y -= 0.1;
+	//	break;
+	//case GLUT_KEY_UP:		moving_sphere.y += 0.1;
+	//	break;
+	default: break;
+	}
+	if (barX < 42)				barX = 42;					// backgroundLeft : 20
+	if (barX > 1058 - barW)		barX = 1058 - barW;			// backgroundRight: 1080		barWidth: 70
 }
 
 
-bool collides(const Ball& ball, const Block& block) {
-	// Check if ball is within block's bounds
+bool collides(const Ball& ball, const Brick& block) {
+	// Check if ball is within brick's bounds
 
 	if (block.point.x <= ball.center.x + ball.radius && ball.center.x + ball.radius < block.point.x + block.width
 		&& block.point.y <= ball.center.y + ball.radius && ball.center.y + ball.radius < block.point.y + block.height
@@ -336,24 +470,24 @@ bool collides(const Ball& ball, const Block& block) {
 	return false;
 }
 
-// Calculate angle of incidence and angle of reflection after collision with block
-void updateBallDirection(Ball& ball, Block& block) {
-	// Calculate the normal vector of the block's surface at the point of collision
+// Calculate angle of incidence and angle of reflection after collision with brick
+void updateBallDirection(Ball& ball, Brick& block) {
+	// Calculate the normal vector of the brick's surface at the point of collision
 
 	//float normX, normY;
-	//if (ball.center.x < block.point.x) {
+	//if (ball.center.x < brick.point.x) {
 	//	normX = -1;
 	//}
-	//else if (ball.center.x > block.point.x + block.width) {
+	//else if (ball.center.x > brick.point.x + brick.width) {
 	//	normX = 1;
 	//}
 	//else {
 	//	normX = 0;
 	//}
-	//if (ball.center.y < block.point.y) {
+	//if (ball.center.y < brick.point.y) {
 	//	normY = -1;
 	//}
-	//else if (ball.center.y > block.point.y + block.height) {
+	//else if (ball.center.y > brick.point.y + brick.height) {
 	//	normY = 1;
 	//}
 	//else {
@@ -389,30 +523,33 @@ void Draw_Score(void) {
 	char str_3[] = "3";
 	char str_4[] = "4";
 	char str_5[] = "5";
-	drawBitmapText(str_1, 520, 610);
-	drawBitmapText(str_2, 520, 580);
-	drawBitmapText(str_3, 520, 550);
-	drawBitmapText(str_4, 520, 520);
-	drawBitmapText(str_5, 520, 490);
 
 	char str_Rank[] = "Rank";
-	drawBitmapText(str_Rank, 570, 650);
+	drawBitmapText(str_Rank, scoreX + scoreW / 3, 600);
+
+	drawBitmapText(str_1, scoreX + scoreW / 4, 550);
+	drawBitmapText(str_2, scoreX + scoreW / 4, 520);
+	drawBitmapText(str_3, scoreX + scoreW / 4, 490);
+	drawBitmapText(str_4, scoreX + scoreW / 4, 460);
+	drawBitmapText(str_5, scoreX + scoreW / 4, 430);
+
 	for (int i = 0; i < 5; i++) {
 		int str_r = ranking[i];
-		drawBitmapText(itoa(str_r, str_Rank, 10), 600, 610 - i * 30);
+		drawBitmapText(itoa(str_r, str_Rank, 10), scoreX + scoreW / 2, 550 - i * 30);
 	}
 
 	char str_Score[] = "Score";
-	drawBitmapText(str_Score, 560, 400);
-	drawBitmapText(itoa(score, str_Score, 10), 580, 350);
+	drawBitmapText(str_Score, scoreX + scoreW * 2 / 5, 350);
+	drawBitmapText(itoa(score, str_Score, 10), scoreX + scoreW * 2 / 5, 300);
 
 
 	char str_Time[] = "Time";
 	char colon[] = ":";
-	drawBitmapText(str_Time, 570, 250);
-	drawBitmapText(itoa(time_limit / 60, str_Time, 10), 560, 200);
-	drawBitmapText(colon, 590, 200);
-	drawBitmapText(itoa(time_limit % 60, str_Time, 10), 610, 200);
+	drawBitmapText(str_Time, scoreX + scoreW * 2 / 5, 200);
+	drawBitmapText(itoa(time_limit / 60, str_Time, 10), scoreX + scoreW / 3, 150);
+	drawBitmapText(colon, scoreX + scoreW / 2, 150);
+	drawBitmapText(itoa(time_limit % 60, str_Time, 10), scoreX + scoreW * 2 / 3, 150);
+
 }
 
 
@@ -441,25 +578,31 @@ void RenderScene(void) {
 	//for (i = 0; i < ball_num; i++) {
 	//	for (int j = 0; j < block_num; j++) {
 	//		//Check_Collision_Ball_Block(i, j);
-	//		if (collides(ball[i], block[j])) {
-	//			updateBallDirection(ball[i], block[j]);
+	//		if (collides(ball[i], brick[j])) {
+	//			updateBallDirection(ball[i], brick[j]);
 	//		}
 	//	}
 	//}
 
 	// cout << "Ball Update" << "\n";
 
+	Check_Collision_Ball_Brick();	
+	for (i = 0; i < ball_num; i++)
+		Check_Collision_Ball_Bar(ball[i]);
 	Update_Position();
 
 	Draw_Background();
+	Draw_Bar();
 
-	Draw_Bar(200, 100);
+
+
+	for (i = 0; i < block_num; i++)
+		Draw_Brick(i);
 
 	for (i = 0; i < ball_num; i++)
 		Draw_Ball(i);
 
-	for (i = 0; i < block_num; i++)
-		Draw_Block(i);
+	
 
 	Draw_Score();
 
